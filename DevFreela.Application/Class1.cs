8442e53f26ac1b1315cd8499cfa58cc3e0749dd7
Priscila <priscilaresantos@gmail.com>
2021-05-15 13:02:20 -0300

    Criação da estrutura do projeto DevFreela

---
+++ DevFreela.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DevFreela.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ DevFreela.Infrastructure.Persistence/DevFreelaDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFreela.Infrastructure.Persistence
+{
+    // Configuração com banco de dados
+
+    class DevFreelaDbContext
+    {
+    }
+}
